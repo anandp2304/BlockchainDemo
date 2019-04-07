55bf267ced33975a72b88c1dc4a2d4138e3bfd19
anandp2304 <anand.p2304@gmail.com>
2019-04-07 21:56:13 +0530

    Add files via upload

---
+++ BlockchainDemo/Blockchain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace BlockchainDemo
+{
+    [Serializable()]
+    public class Blockchain : ISerializable
+    {
+        public IList<Block> Chain { set; get; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void SerializeBlock(Blockchain blk)
+        {
+            Stream stream = File.Open("EmployeeInfo.osl", FileMode.Create);
+            BinaryFormatter bformatter = new BinaryFormatter();
+            Console.WriteLine("Writing Employee Information");
+            bformatter.Serialize(stream, blk);
+            stream.Close();
+        }
+
+        public void Deserialize()
+        {
+            Blockchain strm = new Blockchain();
+            //Open the file written above and read values from it.
+            Stream deSerializeStream = File.Open("EmployeeInfo.osl", FileMode.Open);
+            BinaryFormatter bformatter = new BinaryFormatter();
+            //Console.WriteLine("Reading Employee Information");
+            strm = (Blockchain)bformatter.Deserialize(deSerializeStream);
+            deSerializeStream.Close();
+        }
+    }
+}
---
+++ BlockchainDemo/BlockchainDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockchainDemo
+{
+    public partial class BlockchainDemo : Form
+    {
+        public BlockchainDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Blockchain phillyCoin = new Blockchain();
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
+
+            
+            //Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
+            phillyCoin.SerializeBlock(phillyCoin);
+
+            //Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+
+            //Console.WriteLine($"Update amount to 1000");
+            phillyCoin.Chain[1].Data = "{sender:Henry,receiver:MaHesh,amount:1000}";
+
+            //Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+
+            //Console.WriteLine($"Update hash");
+            phillyCoin.Chain[1].Hash = phillyCoin.Chain[1].CalculateHash();
+
+            //Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+
+            //Console.WriteLine($"Update the entire chain");
+            phillyCoin.Chain[2].PreviousHash = phillyCoin.Chain[1].Hash;
+            phillyCoin.Chain[2].Hash = phillyCoin.Chain[2].CalculateHash();
+            phillyCoin.Chain[3].PreviousHash = phillyCoin.Chain[2].Hash;
+            phillyCoin.Chain[3].Hash = phillyCoin.Chain[3].CalculateHash();
+
+            //Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+
+            Console.ReadKey();
+        }
+    }
+}
